e7eea0a1e4416f1553a205d4ddaebfff2435df6e
Yuri Dias <101608041+YuriDDS@users.noreply.github.com>
2025-05-02 20:48:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ if localizacao:
         icon=icone_personalizado
     ).add_to(m)
 
-    st_folium(m, width="100%", height=700)
+    st_folium(m, width= 100, height=400)
 
     # Chave da API do OpenWeather (configurada no secrets)
     api_key = st.secrets["OPENWEATHER_API_KEY"]
